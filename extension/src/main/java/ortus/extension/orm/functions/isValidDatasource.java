4bcafc62d0b9d286a30f016a4d7b988747482b49
Michael Born <michael@ortussolutions.com>
2023-06-11 05:42:59 -0400

    📦 NEW: Add isValidDatasource() for easy datasource test skips

---
+++ extension/src/main/java/ortus/extension/orm/functions/isValidDatasource.java
@@ -0,0 +1,39 @@
+package ortus.extension.orm.functions;
+
+import lucee.loader.engine.CFMLEngineFactory;
+import lucee.loader.engine.CFMLEngine;
+import lucee.runtime.ext.function.BIF;
+import lucee.runtime.util.Cast;
+import lucee.runtime.PageContext;
+import lucee.runtime.exp.PageException;
+import lucee.runtime.db.DataSource;
+import lucee.runtime.db.DataSourceManager;
+import lucee.runtime.db.DatasourceConnection;
+
+public class isValidDatasource extends BIF {
+
+    public static Boolean call(PageContext pc, String name) throws PageException {
+        DataSource d = pc.getDataSource(name);
+		DataSourceManager manager = pc.getDataSourceManager();
+
+        try {
+            DatasourceConnection conn = manager.getConnection(pc, d, d.getUsername(), d.getPassword());
+			manager.releaseConnection(pc, conn);
+			return true;
+		}
+		catch (PageException e) {
+			return false;
+		}
+    }
+
+    @Override
+    public Object invoke(PageContext pc, Object[] args) throws PageException {
+        CFMLEngine engine = CFMLEngineFactory.getInstance();
+        Cast cast = engine.getCastUtil();
+
+        if (args.length == 1)
+            return call(pc, cast.toString(args[0]));
+
+        throw engine.getExceptionUtil().createFunctionException(pc, "isValidDatasource", 1, 1, args.length);
+    }
+}
