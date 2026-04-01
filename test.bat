a5087729c01ca7810bc6c4439d8f6e62a4140963
Zac Spitzer <zac.spitzer@gmail.com>
2026-04-01 16:51:51 +0200

    Migrate build from Ant to Maven, shade all deps into fat jar
    Replace Ant+vendored-jars build with pure Maven. All dependencies
    resolved from Maven Central, shaded into a single OSGi bundle.
    Removes 22 vendored jars from source/java/libs/.

--- test.bat
+++ test.bat
@@ -1,11 +1,10 @@
 SET "JAVA_HOME=C:\Program Files\Eclipse Adoptium\jdk-11.0.29.7-hotspot"
-call mvn package
+call mvn verify
 if %errorlevel% neq 0 exit /b %errorlevel%
 set testLabels=orm
 set testFilter=
 set testAdditional=d:\work\lucee-extensions\extension-hibernate\tests
 set testServices=mysql,mssql
-
 
 call ant -buildfile "d:\work\script-runner" -DluceeVersion="7.0/snapshot/light" -Dwebroot="d:\work\lucee7" -Dexecute="test\bootstrap-tests.cfm" -DextensionDir="d:\work\lucee-extensions\extension-hibernate\target" -DuniqueWorkingDir="true"
 
